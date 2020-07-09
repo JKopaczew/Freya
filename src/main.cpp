264469ceabc8137d8dd92b309d42eec3402f8907
Jaroslaw Kopaczewski <50420430+JKopaczew@users.noreply.github.com>
2020-07-09 12:40:33 +0100

    initial commit
    Basic Cmake set up
    Builds GLFW from source
    Links to Vulkan SDK successfully

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <vulkan/vulkan.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+
+    GLFWwindow* window;
+    
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    std::cout << glfwVulkanSupported() << std::endl;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
