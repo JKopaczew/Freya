5d1dbe5d528f81bf37192176e11299de893c8af0
Jaroslaw Kopaczewski <50420430+JKopaczew@users.noreply.github.com>
2020-07-09 16:50:15 +0100

    Re-branding
    Fixed and updated README
    Updated Cmake
    Cleaned up main function

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# RainEngine
-A rendering engine based on a the Vulan api 
+# Freya engine
+A rendering engine based on a the Vulkan api
+
+##Purpose
+Freya doesnt have a solid goal outside of being a project to learn the Vulkan api, 
+basically something resebling a basic game engine <br/>
+
+##External resources
+
+    Vulkan SDK
+    GLFW
+    
+ 
+ 
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,44 @@
 #include <vulkan/vulkan.h>
-#include <iostream>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
+    int W = 640;
+    int H = 480;
+    const char *Title= "Freya";
 
     GLFWwindow* window;
     
-    /* Initialize the library */
+    // Initialize the library
     if (!glfwInit())
         return -1;
+
+    // Check if Vulkan is supported, will return 1 to console on success
     std::cout << glfwVulkanSupported() << std::endl;
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(W, H, Title, NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
     }
 
