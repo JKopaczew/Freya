3ad3ebe2b3be1a0c71fba014a0f22fd5269828ee
Jaroslaw Kopaczewski <50420430+JKopaczew@users.noreply.github.com>
2020-07-16 20:23:42 +0100

    GLFW integration
    Successfully created a window using glfw

--- src/Freya.cpp
+++ src/Freya.cpp
@@ -1,6 +1,38 @@
+#include <assert.h>
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	printf("Hello, Vulkan!");
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    int rc = glfwInit();
+    assert(rc);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1024, 768, "Freya", NULL, NULL);
+    assert(window);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
