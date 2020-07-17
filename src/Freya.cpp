42b50d8e361a600eda1519d1e26181cd2d4f2056
Jaroslaw Kopaczewski <50420430+JKopaczew@users.noreply.github.com>
2020-07-17 23:39:44 +0100

    Clean up

--- src/Freya.cpp
+++ src/Freya.cpp
@@ -1,21 +1,34 @@
 #include <assert.h>
-#include <stdio.h>
+
 #include <GLFW/glfw3.h>
+
 #include <vulkan/vulkan.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+
+//#define VK_CHECK(call) do{VkResult result_ = call; assert(result_ == VK_SUCCESS);} while (0);
 
 int main(void)
 {
-	GLFWwindow* window;
-
 	// Initialize glfw
-	int rc = glfwInit();
-	assert(rc);
-
-	vkCreateInstance();
+	glfwInit();
+	VkInstanceCreateInfo createInfo{};
+	
+	VkInstance instance = 0;
+	//VK_CHECK(ckCreateInstance(&createInfo, 0, &instance));
 
 	// Create a windowed mode window and its OpenGL context
-	window = glfwCreateWindow(1024, 768, "Freya", NULL, NULL);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Freya", NULL, NULL);
 	assert(window);
+
+
 	if (!window)
 	{
 		glfwTerminate();
