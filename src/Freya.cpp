2c74845f7bf42a707113a7a02fcaed41d1b9b129
Jaroslaw Kopaczewski <JKopa@users.noreply.github.com>
2020-07-18 21:47:36 +0100

    output available extensions
    more work on creating a Vulkan instance and now outputs all avaialbe extensions to the console uppon creation of said intance

--- src/Freya.cpp
+++ src/Freya.cpp
@@ -1,6 +1,7 @@
-#include <vulkan/vulkan.h>
-#include <GLFW/glfw3.h>
+//#include <vulkan/vulkan.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -10,61 +11,110 @@
 #include <iostream>
 #include <stdexcept>
 #include <assert.h>
+#include <vector>
 
-class HelloTriangleApp {
+const uint32_t Width = 1024;
+const uint32_t Height = 768;
+
+class HelloTriangle
+{
 public:
 	void run()
 	{
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	GLFWwindow* window;
+	VkInstance instance;
+	void initWindow()
+	{
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		//GLFW CODE
+		// Create a windowed mode window and its OpenGL context
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		window = glfwCreateWindow(Width, Height, "Freya", NULL, NULL);
+		assert(window);
+	}
+
 	void initVulkan()
 	{
-
+		createInstance();
 	}
 
 	void mainLoop()
 	{
-
+		// Loop until the user closes the window
+		while (!glfwWindowShouldClose(window))
+		{
+			// Poll for and process events
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup()
 	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 
+	void createInstance() {
+		uint32_t extensionCount = 0; //set default value of extension count that will be passed to vkEnumerateInstanceExtensionProperties
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Freya";
+		appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+		appInfo.pEngineName = "Freya";
+		appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_2;
+
+
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance)!= VK_SUCCESS) 
+		{
+			throw std::runtime_error("failed to create vulkan instance");
+		}
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		std::cout << "available extensions:\n";
+
+		for (const auto& extension : extensions) 
+		{
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+		
 	}
 };
 
 int main()
 {
+	
+	HelloTriangle triangle;
 
-	//GLFW CODE
-	// Create a windowed mode window and its OpenGL context
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(1024, 768, "Freya", NULL, NULL);
-	assert(window);
-
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	// Make the window's context current
-	glfwMakeContextCurrent(window);
-	// Loop until the user closes the window
-	while (!glfwWindowShouldClose(window))
-	{
-		// Swap front and back buffers
-		glfwSwapBuffers(window);
-
-		// Poll for and process events
-		glfwPollEvents();
-	}
-	glfwTerminate();
-
+	triangle.run();
 
 	return 0;
 }
